6a606d7cb2c0fe038aa8b00ae444df5af0960e67
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-04-02 22:46:39 -0300

    added db connection

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const connectDB = require("./connectDB")
 const app = express();
+const Tasks = require('./models/Tasks')
 const PORT  = process.env.PORT || 8000;
+connectDB();
+app.use(cors())
+
+app.use(express.urlencoded( {extended: true}))
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.json("Hello Mate!")
+    res.json(" Hello mate!")
 })
+app.get('*', (req, res) => {
+ res.sendStatus("404")
+})
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`);
